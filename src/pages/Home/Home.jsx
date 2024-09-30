38b5e2305b4af034b5023918d1e1d021068ede79
anagh534 <anaghkrkkl@gmail.com>
2024-09-30 10:56:46 +0530

    inital commit

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavBar from '../../components/NavBar'
+import  './Home.css'
+
+function Home() {
+  return (
+    <>
+      <NavBar />
+      <div>Home</div>
+    </>
+  )
+}
+
+export default Home
