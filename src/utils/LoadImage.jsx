4fd3bac775a3161cc13fef08a49f6f6dbe47e158
anagh534 <anaghkrkkl@gmail.com>
2024-10-17 10:38:48 +0530

    new project and project images added

--- src/utils/LoadImage.jsx
+++ src/utils/LoadImage.jsx
@@ -1,5 +1,8 @@
 import image1 from '../assets/projects/1.png';
 import image2 from '../assets/projects/2.png';
+import image3 from '../assets/projects/3.png';
+import image4 from '../assets/projects/4.png';
+import image5 from '../assets/projects/5.png';
 
 const images = [
     {
@@ -9,6 +12,18 @@ const images = [
     {
         img: image2,
         url: 'https://easy-cart-in.glitch.me/'
+    },
+    {
+        img: image3,
+        url: 'https://share-files-in.glitch.me/'
+    },
+    {
+        img: image4,
+        url: 'https://sa-adiyacollege.com/'
+    },
+    {
+        img: image5,
+        url: 'https://anagh534.github.io/music/'
     }
 ];
 
